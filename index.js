53bb3c3b1cf8f9cbbd936fb6474248f03900ae0b
Ziyu Song <songziyu98@gmail.com>
2019-04-05 00:59:10 -0400

    Updated index.js
    User input of game name from index.html now is appended to request to eBay API as search keywords.

--- index.js
+++ index.js
@@ -4,26 +4,43 @@ var request = new XMLHttpRequest();
 var searchBar = document.getElementById("searchBar");
 var gameName = "";
 var results = document.getElementById("results");
+var returned_data;
+var returned_items;
 
 function search() {
     gameName = searchBar.value;
-    fetch();
     results.innerHTML = "<p>Searched for " + gameName + ".</p>";
+    keywords = gameName.replace(" ", "+");
+    url += "&keywords=" + keywords;
+
+	//create script element
+ 	s = document.createElement('script'); 
+ 	s.src = url;
+	document.body.appendChild(s);
 }
 
 
+////////////////////////////////////////////////////////////////////////////
+///////////////////////  eBay API Search Filters ///////////////////////////
+////////////////////////////////////////////////////////////////////////////
 
 //https://developer.ebay.com/devzone/finding/CallRef/findCompletedItems.html
 
 // request URL
-var url = "http://svcs.ebay.com/services/search/FindingService/v1" + "?";
-
-// filters
-url += "OPERATION-NAME=findCompletedItems";
+var url = "http://svcs.ebay.com/services/search/FindingService/v1";
+url += "?OPERATION-NAME=findCompletedItems";
 url += "&SERVICE-VERSION=1.13.0";
 url += "&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD";
-url += "&keywords=Garmin+nuvi+1300+Automotive+GPS+Receiver";
-url += "&categoryId=156955&itemFilter(0).name=Condition&itemFilter(0).value=3000&itemFilter(1).name=FreeShippingOnly&itemFilter(1).value=true&itemFilter(2).name=SoldItemsOnly&itemFilter(2).value=true&sortOrder=PricePlusShippingLowest&paginationInput.entriesPerPage=2";
+
+// video games category id : 139973
+url += "&categoryId=139973";
+
+// item filers
+url += "&itemFilter(0).name=FreeShippingOnly&itemFilter(0).value=true&itemFilter(1).name=SoldItemsOnly&itemFilter(1).value=true";
+url += "&itemFilter(2).name=TopRatedSellerOnly&itemFilter(2).value=true";
+// sort order
+
+url += "&sortOrder=BestMatch";
 
 // ebay access key
 url += "&SECURITY-APPNAME=ZiyuSong-ValueStr-PRD-279703086-b2b1a6a0";
@@ -31,9 +48,7 @@ url += "&SECURITY-APPNAME=ZiyuSong-ValueStr-PRD-279703086-b2b1a6a0";
 // callback function name
 url += "&callback=fetch";
 function fetch(data) {
-    console.log(data);
+	returned_items = data.findCompletedItemsResponse[0].searchResult[0].item;
+	console.log(returned_items);
 }
 
-s = document.createElement('script'); // create script element
-s.src= url;
-document.body.appendChild(s);
