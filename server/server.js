2508f038d228608f3b97e8f96090aadbaa041769
Zimo Lin <zimolin@Zimos-MacBook-Pro.local>
2019-04-11 13:51:43 -0400

    server added

---
+++ server/server.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+var validator = require ('validator');
+var app = express();
+const path = require('path');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+var request = new XMLHttpRequest();
+
+var url = "http://svcs.ebay.com/services/search/FindingService/v1";
+url += "?OPERATION-NAME=findCompletedItems";
+url += "&SERVICE-VERSION=1.13.0";
+url += "&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD";
+
+// video games category id : 139973
+url += "&categoryId=139973";
+
+// item filers
+url += "&itemFilter(0).name=FreeShippingOnly&itemFilter(0).value=true&itemFilter(1).name=SoldItemsOnly&itemFilter(1).value=true";
+url += "&itemFilter(2).name=TopRatedSellerOnly&itemFilter(2).value=true";
+// sort order
+
+url += "&sortOrder=BestMatch";
+
+// ebay access key
+url += "&SECURITY-APPNAME=ZiyuSong-ValueStr-PRD-279703086-b2b1a6a0";
+
+// callback function name
+url += "&callback=fetch";
+
+const PORT = process.env.PORT || 5000
+const https = require('https');
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
+app.post('/search', function(req, res){
+  if (req.body.hasOwnProperty('keywords')) {
+    url += "&keywords=" + keywords;
+    https.get(url, function(httpRes){
+      console.log(httpRes);
+      res.send(httpRes);
+    })
+  } else 
+    res.send({"error" : "Something is wrong with the data"});   
+});
