852432130836a7a11c7b0fde3ba35230d9dfe032
WillV <46897580+AQuin1904@users.noreply.github.com>
2019-04-29 00:10:35 -0400

    CSS fixes
    Updates CSS for better mobile compatibility. Also makes another attempt at database connection issue.

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const mongoURI = process.env.MONGODB_URI;
 const MongoClient = require('mongodb').MongoClient, format = require('util').format;
 var db = MongoClient.connect(mongoURI, (err, client) => {
   if(err) throw err;
-  db = client.db('valuestreak');
+  db = client.db('heroku_jsrqth5');
 });
 const collName = 'games';
 
