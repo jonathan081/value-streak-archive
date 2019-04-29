5053d360872116946d9c8e2b4227dee0f73986a9
WillV <46897580+AQuin1904@users.noreply.github.com>
2019-04-29 15:00:47 -0400

    Updates chart styling
    Updates the styling elements for the google chart. Also re-enables https redirect.

--- public/index.js
+++ public/index.js
@@ -142,8 +142,8 @@ function drawChart(prices) {
           title: 'Price History of ' + gameName,
           curveType: 'function',
           colors: ['#000000'],
-          'width': 800,
-          'height': 500,
+          //'width': 800,
+          //'height': 500,
           vAxis: {viewWindow: { min: 0}}
         };
     chart.draw(data, options);
