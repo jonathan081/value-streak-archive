652329c7cb71dbdade4ead331a9162acf124c8ce
WillV <46897580+AQuin1904@users.noreply.github.com>
2019-04-29 09:29:21 -0400

    Changes database query
    Changes check on database query in server.js in effort to resolve issue of items not being inserted.

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ app.post('/search', (req, res) => {
             "lastAvg": lastAvg,
             };
 
-            if(old) {
+            if(old.hasOwnProperty('last') && old.hasOwnProperty('oldest')) {
               toSend.oldestAvg = old.oldest;
               toSend.lastAvg = old.last;
               coll.updateOne({'title': key}, {$set: {'last': toUpdate}});
